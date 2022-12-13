0399b7ffa09e4323b058044a382be87f481b4410
flucrum <flucrum@gmail.com>
2022-12-14 00:57:06 +0500

    Replase lexer class as type

--- js/src/lexer.js
+++ js/src/lexer.js
@@ -1,19 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseLoadFromConfig = exports.LuxLangLexer = void 0;
+exports.parseLoadFromConfig = void 0;
 var configModule = require("./config");
 var tokenLoaderModule = require("./lexer/token-loader");
-var LuxLangLexer = /** @class */ (function () {
-    function LuxLangLexer(tokenDefs) {
-        this.tokenDefs = tokenDefs;
-    }
-    return LuxLangLexer;
-}());
-exports.LuxLangLexer = LuxLangLexer;
 function parseLoadFromConfig(configPath, coding) {
     if (coding === void 0) { coding = 'utf8'; }
     var config = configModule.loadFromFile(configPath);
     var tokenDefs = tokenLoaderModule.load(configPath, config.tokenDefsFilePaths);
-    return new LuxLangLexer(tokenDefs);
+    return { tokenDefs: tokenDefs };
 }
 exports.parseLoadFromConfig = parseLoadFromConfig;
--- ts/src/lexer.ts
+++ ts/src/lexer.ts
@@ -4,19 +4,13 @@ import * as configModule from "./config";
 import * as tokenDefModule from "./lexer/token-def";
 import * as tokenLoaderModule from "./lexer/token-loader";
 
-
-export class LuxLangLexer 
-{
-    public tokenDefs: Array<tokenDefModule.TokenDef>;
-
-    constructor(tokenDefs: Array<tokenDefModule.TokenDef>) {
-        this.tokenDefs = tokenDefs;
-    }
+export type LuxLangLexer = {
+    tokenDefs: Array<tokenDefModule.TokenDef>;
 }
 
 export function parseLoadFromConfig(configPath: string, coding: string = 'utf8'): LuxLangLexer
 {
     const config = configModule.loadFromFile(configPath);
     const tokenDefs = tokenLoaderModule.load(configPath, config.tokenDefsFilePaths);
-    return new LuxLangLexer(tokenDefs);
+    return {tokenDefs: tokenDefs} as LuxLangLexer;
 }
