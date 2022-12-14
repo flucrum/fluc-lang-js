a71fb2b330d619cc47402e219bd73ad5d0a6a518
flucrum <flucrum@gmail.com>
2022-12-15 03:55:09 +0500

    Add math operations tokens Add new test nominal phrase and tests for it

--- js/test/tokens-collection.test.js
+++ js/test/tokens-collection.test.js
@@ -5,11 +5,17 @@ var lexerModule = require("../src/lexer");
 var pathModule = require("path");
 var fsModule = require("fs");
 var lexer = lexerModule.parseLoadFromConfig(pathModule.resolve(__dirname, "../../lexics/config.json"));
-describe("Test base syntax", function () {
+describe("Test syntax", function () {
     it("nominal1", function () {
         var text = fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/1/phrase.fluc"), "utf8");
         var resultTokens = lexerModule.analyzeText(text, lexer);
         var nominalTokens = JSON.parse(fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/1/tokens.json"), "utf8"));
+        assertModule.deepEqual(nominalTokens, resultTokens);
+    });
+    it("nominal2", function () {
+        var text = fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/2/phrase.fluc"), "utf8");
+        var resultTokens = lexerModule.analyzeText(text, lexer);
+        var nominalTokens = JSON.parse(fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/2/tokens.json"), "utf8"));
         assertModule.deepEqual(nominalTokens, resultTokens);
     });
 });
--- ts/test/tokens-collection.test.ts
+++ ts/test/tokens-collection.test.ts
@@ -5,12 +5,21 @@ import * as fsModule from "fs";
 
 const lexer = lexerModule.parseLoadFromConfig(pathModule.resolve(__dirname, "../../lexics/config.json"));
 
-describe("Test base syntax", () => {
+describe("Test syntax", () => {
     it("nominal1", () => {
         let text = fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/1/phrase.fluc"), "utf8");
         let resultTokens = lexerModule.analyzeText(text, lexer);
         let nominalTokens = JSON.parse(
             fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/1/tokens.json"), "utf8")
+        );
+        assertModule.deepEqual(nominalTokens, resultTokens);
+    });
+
+    it("nominal2", () => {
+        let text = fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/2/phrase.fluc"), "utf8");
+        let resultTokens = lexerModule.analyzeText(text, lexer);
+        let nominalTokens = JSON.parse(
+            fsModule.readFileSync(pathModule.resolve(__dirname, "../../nominals/2/tokens.json"), "utf8")
         );
         assertModule.deepEqual(nominalTokens, resultTokens);
     });
